586a8bad76dd3f4940026a7fd54042231dc078c8
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-10-22 14:23:02 +0100

    update proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: " https://hi-social-app-server.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
